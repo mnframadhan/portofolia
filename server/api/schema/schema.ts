2d34d24123deefdc62500e7f76d843168895a98a
MNF Ramadhan <mnfitrahramadhan@gmail.com>
2024-10-07 23:43:37 +0700

    authentication

--- server/api/schema/schema.ts
+++ server/api/schema/schema.ts
@@ -1,12 +1,5 @@
 import { pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const testUser = pgTable('testusers', {
-    id: varchar('id', {length: 256}).primaryKey(),
-    username: varchar('username', {length: 256}),
-    phone: varchar('phone', {length: 256}),
-    avatar: varchar('avatar', {length: 256}),
-});
-
 export const users = pgTable('users', {
 
     id: varchar('id', {length: 256} ).notNull().primaryKey(),
